fc3c5076bbcbe63bc62814e20a8a3864cd957b99
Alexander Bisono <sbisonol@gmail.com>
2021-10-16 15:59:26 -0400

    Add copyright notices and COPYING files

--- include/journalpp/journalpp.h
+++ include/journalpp/journalpp.h
@@ -7,6 +7,23 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <string>
+/*
+ * Copyright (C) 2021 Alexander Bisono.
+ *
+ * This file is part of libjournalpp.
+ *
+ * libjournalpp is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * libjournalpp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with libjournalpp. If not, see <https://www.gnu.org/licenses/>.
+ * */
 
 namespace journalpp {
     int foo();
--- src/lib.cpp
+++ src/lib.cpp
@@ -10,6 +10,23 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+/*
+ * Copyright (C) 2021 Alexander Bisono.
+ *
+ * This file is part of libjournalpp.
+ *
+ * libjournalpp is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * libjournalpp is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with libjournalpp. If not, see <https://www.gnu.org/licenses/>.
+ * */
 
 int journalpp::foo() {
     return 1;
