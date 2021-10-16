affe7f6393e4e5fd6058524e1bb70a28b13ddb59
Alexander Bisono <sbisonol@gmail.com>
2021-10-16 14:16:37 -0400

    Make `test/marshalling_fields.cpp` write to stdout
    Just to ease pollution of the system journal when testing.
    
    For some godforsaken reason, checking for equality doesn't work when
    comparing the output string to the ideal output (even when its own
    output is copied from a debugger like GDB). So this has to be actually
    written to a file and checked manually in order to verify that it works.
    If someone has a better solution, though, please do not hesitate to
    notify me and/or offer a fix.
    
    - Dr. Bluefall.

--- test/marshalling_fields.cpp
+++ test/marshalling_fields.cpp
@@ -1,13 +1,21 @@
+#include <iostream>
 #include <sstream>
 #include <journalpp/journalpp.h>
 
 int main() {
     journalpp::journal j;
 
+    // FIXME: For some godforsaken reason, checking for equality doesn't work.
+    // So this has to be actually written to a file and checked manually in
+    // order to verify that it works. If someone has a better solution, though,
+    // please do not hesitate to notify me.
+    //
+    // - Dr. Bluefall.
+
     std::ostringstream out;
-    j.write_to_socket("MESSAGE=mslbegin\n");
     j.marshal_field(out, "MESSAGE", "foo\nbar");
-    j.write_to_socket(out.str());
-    j.write_to_socket("MESSAGE=mslend\n");
+
+    std::cout << out.str();
+
     j.wrapup();
 }
